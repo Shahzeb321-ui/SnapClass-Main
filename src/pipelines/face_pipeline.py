fd9a52512a21630244078038ace3654ee59d1cd5
Muhammad Shahzeb Sheraz <rajashahzaib3488@gmail.com>
2026-08-26 13:31:10 +0500

    last finished

--- src/pipelines/face_pipeline.py
+++ src/pipelines/face_pipeline.py
@@ -64,34 +64,35 @@ def train_classifier():
     model_data=get_trained_model()
     return bool(model_data)
 
-def predict_attendence(class_image_np):
-    encodings=get_face_embeddings(class_image_np)
-    detected_students={}
+def predict_attendance(class_image_np):
+    encodings = get_face_embeddings(class_image_np)
 
-    model_data=get_trained_model()
+    detected_student = {}
+
+
+    model_data = get_trained_model()
+
     if not model_data:
-        return detected_students,[],len(encodings)
+        return detected_student, [], len(encodings)
+    
+    clf = model_data['clf']
+    X_train = model_data['X']
+    y_train = model_data['y']
 
-    clf=model_data['clf']
-    X_train=model_data['X']
-    Y_train=model_data['Y']
-
-
-    all_students=sorted(list(set(Y_train)))
+    all_students = sorted(list(set(y_train)))
 
     for encoding in encodings:
-        if len(all_students)>=2:
-            predicted_id=int(clf.predict([encoding])[0])
+        if len(all_students)>= 2:
+            predicted_id= int(clf.predict([encoding])[0])
         else:
-            predicted_id=int(all_students[0])
+            predicted_id = int(all_students[0])
 
-        student_embedding=X_train[Y_train.index[predicted_id]]
+        student_embedding = X_train[y_train.index(predicted_id)]
 
-        best_match_score=np.linalg.norm(student_embedding-encoding)
-        resemblance_threshold=0.6
+        best_match_score = np.linalg.norm(student_embedding - encoding)
 
-        if best_match_score<=resemblance_threshold:
-            detected_students[predicted_id]=True
-    return detected_students,all_students,len(encodings)
-    
-    
+        resemblance_threshold = 0.6
+
+        if best_match_score <= resemblance_threshold:
+            detected_student[predicted_id] = True
+    return detected_student, all_students, len(encodings)
